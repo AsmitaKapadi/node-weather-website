55931f9c8908f5351e6ba51f53baaa1cd16b43cc
Asmita Kapadi <asmita.kapadi@ext.saint-gobain.com>
2022-03-20 11:59:58 +0530

    Init Commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,9 @@
 
-console.log('Client side js')
-
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
+/* fetch('http://puzzle.mead.io/puzzle').then((response) => {
     response.json().then((data) => {
         console.log(data)
     })
-})
+}) */
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
